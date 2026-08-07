9689e3533415a1ac693977c2b10925af9d167300
ramazef <ramrodriguez@frba.utn.edu.ar>
2026-08-06 23:39:50 -0300

    Arranque paradigma logico

---
+++ Paradigma_Logico.pl
@@ -0,0 +1,39 @@
+
+
+% Hechos
+animal(perro).
+animal(gato).
+animal(caballo).
+
+edad(perro, 5).
+edad(gato, 3).
+edad(caballo, 10).
+
+
+% FINDALL
+% Buscamos todos los animales y los guardamos en una lista.
+% Consulta:
+% ?- findall(X, animal(X), Animales).
+%
+% Resultado:
+% Animales = [perro, gato, caballo].
+
+
+% Otro ejemplo:
+% Buscamos todas las edades.
+% ?- findall(Edad, edad(Animal, Edad), Edades).
+%
+% Resultado:
+% Edades = [5, 3, 10].
+
+
+% Podemos usar findall dentro de una función:
+cantidadAnimales(Cantidad) :-
+    findall(X, animal(X), Animales),
+    length(Animales, Cantidad).
+
+% Consulta:
+% ?- cantidadAnimales(Cantidad).
+%
+% Resultado:
+% Cantidad = 3.
