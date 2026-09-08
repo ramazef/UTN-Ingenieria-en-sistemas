ba3e926cf224968514a7181d7556880c19ac51be
ramazef <ramrodriguez@frba.utn.edu.ar>
2026-09-08 20:15:41 -0300

    Ejercicios de parcial logico

---
+++ Ejercicios_Parcial.pl
@@ -0,0 +1,52 @@
+
+
+% Segundo requerimiento: lenguaje copado
+
+% Aquel en el que todas las personas que lo programan son las ideales: 
+% ganan bastante (más de 100 mangos la hora) o programan bien.
+ 
+
+programaEn(nahuel, javascript).
+programaEn(juan, haskell).
+programaEn(juan, prolog).
+programaEn(caro, prolog).
+programaEn(valen, prolog).
+programaEn(hernan, smalltalk).
+
+programaBien(juan).
+programaBien(valen).
+
+gana(caro, 150).
+gana(juan, 40).
+gana(valen, 90).
+gana(hernan, 80).
+
+%el output se que va a ser una consulta existencial que devuelva a prolog xq caro gana bien y haskell pq solo juan programa en ese lenguaje y lo hace bien.
+%Es un OR, voy a tener 2 reglas. Las reglas deben tener el mismo nombre.
+
+% forall( p, q )
+% forall(predicadoQuemevaAservirParaArmarElUniverso, PredicadoQueEstableceCondicionesACumplir)
+
+
+% lenguajeCopado(Lenguaje) es cierto si:
+% - Lenguaje es un lenguaje válido (generador)
+% - Y TODAS las personas que programan en ese lenguaje son ideales
+
+lenguajeCopado(Lenguaje) :-                %tiene un parámetro porque la relación "ser copado" es una propiedad de un lenguaje
+    lenguaje(Lenguaje),                    %Que es lo que queremos ligar antes del forall? Seteo el universo con el que quiero trabajar(generador). Su único trabajo es: "dame todos los lenguajes que existen en la base, uno por uno"
+    forall(programaEn(Persona, Lenguaje),
+           personaIdeal(Persona)).          %Use un predicado auxiliar para modelizar una persona ideal
+
+% lenguaje(Lenguaje) genera cada lenguaje una sola vez (sin repetidos),
+% usando distinct/2 sobre los que aparecen en programaEn/2.
+% Esto es lo que le da el generador a lenguajeCopado para poder usar forall.
+lenguaje(Lenguaje) :-
+    distinct(Lenguaje,
+             programaEn(_, Lenguaje)).
+
+% personaIdeal(Persona) es cierto si gana más de 100 por hora...
+personaIdeal(Persona) :- gana(Persona, Monto),
+    Monto > 100.
+
+% ...o si programa bien (OR armado con 2 cláusulas separadas)
+personaIdeal(Persona) :- programaBien(Persona).
